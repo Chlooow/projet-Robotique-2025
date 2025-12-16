73abf3940a1d0b67a16519f3945e380db20b5b5e
Tugba BULUT <tbulut1558@gmail.com>
2025-12-16 14:21:56 +0100

    Add files via upload

--- object_detector.py
+++ object_detector.py
@@ -16,8 +16,7 @@ class DetecteurObjet:
         self.largeur_image = 640
         self.seuil_aire_min = 50
         self.seuil_erreur_ignore = 2
-
-        self.target_locked = False  # verrouillage de la cible actuelle
+        self.target_locked = False
 
         self.pub_erreur = rospy.Publisher('/object_error', Float32, queue_size=1)
         self.pub_taille = rospy.Publisher('/object_size', Float32, queue_size=1)
@@ -33,7 +32,6 @@ class DetecteurObjet:
 
         hsv = cv2.cvtColor(image_cv, cv2.COLOR_BGR2HSV)
 
-        # Filtre couleur rouge
         bas_rouge_1 = np.array([0, 100, 100])
         haut_rouge_1 = np.array([10, 255, 255])
         bas_rouge_2 = np.array([170, 100, 100])
@@ -49,37 +47,25 @@ class DetecteurObjet:
             valid_contours = [c for c in contours if cv2.contourArea(c) > self.seuil_aire_min]
 
             if valid_contours:
-                # Sélection du plus petit contour (objet le plus éloigné)
                 plus_petit_contour = min(valid_contours, key=cv2.contourArea)
                 aire = cv2.contourArea(plus_petit_contour)
 
                 M = cv2.moments(plus_petit_contour)
                 if M["m00"] > 0:
                     cx = int(M['m10'] / M['m00'])
-
                     erreur_brute = cx - (self.largeur_image / 2)
+                    
                     if abs(erreur_brute) > self.seuil_erreur_ignore:
                         erreur = erreur_brute
 
                     taille = aire
+                    self.target_locked = True
 
-                    # Verrouillage : une fois que l'objet est détecté, on ignore les autres
-                    if not self.target_locked:
-                        self.target_locked = True
-
-                    cv2.drawContours(image_cv, [plus_petit_contour], -1, (0, 255, 0), 2)
-                    cv2.circle(image_cv, (cx, int(image_cv.shape[0] / 2)), 5, (0, 0, 255), -1)
-
-        else:
-            if not self.target_locked:
-                erreur = 0.0
-                taille = 0.0
-
-        # Publication
         self.pub_erreur.publish(Float32(erreur))
         self.pub_taille.publish(Float32(taille))
 
-        cv2.imshow("Masque Rouge", masque)
+        masque_petit = cv2.resize(masque, (320, 240))
+        cv2.imshow("Masque Rouge", masque_petit)
         cv2.waitKey(3)
 
 def main():
@@ -91,4 +77,3 @@ if __name__ == '__main__':
         main()
     except rospy.ROSInterruptException:
         pass
-
